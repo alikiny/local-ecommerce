64782ceb008baae412789c3f98b15f6ca1e2d186
Duy Nguyen <nguyenduy010798@gmail.com>
2022-03-14 13:43:04 +0200

    update

--- src/server.ts
+++ src/server.ts
@@ -13,24 +13,27 @@ mongoose
     useFindAndModify: false,
     useCreateIndex: true,
   })
-  .then(() => {
-    // Start Express server
-    app.listen(app.get('port'), () => {
-      console.log(
-        '  App is running at http://localhost:%d in %s mode',
-        app.get('port'),
-        app.get('env')
-      )
-      console.log('  Press CTRL-C to stop\n')
-    })
-  })
   .catch((err: Error) => {
     console.log(
       'MongoDB connection error. Please make sure MongoDB is running. ' + err
     )
     process.exit(1)
   })
+
 /**
- * Error Handler. Provides full stack - remove for production
+ * Error Handler. Provides error handing middleware
+   only use in development
  */
-app.use(errorHandler())
+if (process.env.NODE_ENV === 'development') {
+  app.use(errorHandler())
+}
+
+// Start Express server
+app.listen(app.get('port'), () => {
+  console.log(
+    '  App is running at http://localhost:%d in %s mode',
+    app.get('port'),
+    app.get('env')
+  )
+  console.log('  Press CTRL-C to stop\n')
+})
